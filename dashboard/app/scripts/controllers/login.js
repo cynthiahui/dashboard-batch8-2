68789e130937f742b49b109a19280c4262c99668
unknown <hui.lavender@gmail.com>
2015-11-06 09:40:58 -0500

    router update

--- dashboard/app/scripts/controllers/login.js
+++ dashboard/app/scripts/controllers/login.js
@@ -1,6 +1,5 @@
-// var app = angular.module("LoginApp", []);
-app.controller("LoginCtrl", ['$scope', 'LoginService', 'GetUserService',
-    function($scope, loginService, getUserService) {
+app.controller("LoginCtrl", ['$scope', '$location', 'LoginService', 'GetUserService',
+    function($scope, $location, loginService, getUserService) {
         $scope.visible = false;
         $scope.errorMsg = '';
         $scope.userName = "darth";
@@ -15,7 +14,23 @@ app.controller("LoginCtrl", ['$scope', 'LoginService', 'GetUserService',
             $scope.$on('login', function(event, userName) {
                 getUserService.getUser($scope, userName);
             });
-            
+            $scope.$on("getUser", function(event, user) {
+                setUser($scope, user);
+                $location.path("/root");
+                $('body').removeClass('login-body');
+            });
         }
     }
 ]);
+
+function setUser(scope, user) {
+    scope.WriteCookie(user);
+}
+
+function getUser(scope, user) {
+    return scope.ReadCookie(user);
+}
+
+function removeUser(scope, user) {
+    scope.RemoveCookie(user);
+}
--- dashboard/app/scripts/services/getUser.js
+++ dashboard/app/scripts/services/getUser.js
@@ -1,6 +1,4 @@
 app.factory("GetUserService", function($http) {
-    // var deferred = $q.defer();
-
     return {
         getUser: function(scope, reqData) {
             $http({
@@ -8,18 +6,10 @@ app.factory("GetUserService", function($http) {
                 url: 'http://localhost:3000/api/getuser',
                 data: reqData
             }).then(function successCallback(success) {
-                // scope.errorMsg = "";
-                // scope.visible = false;
-                // console.log(success.data.name);
                 scope.$emit('getUser', {
                     user:success.data.name
                 });
-                // return success.data.authentication;
-                // return deferred.resolve(success.data.authentication);
             }, function errorCallback(error) {
-                // console.log("nogetUset");
-                // scope.errorMsg = "incorrect information, please try again";
-                // scope.visible = true;
             });
             return "getUser";
         }
