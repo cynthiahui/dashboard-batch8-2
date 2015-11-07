63a934315b35417cb6aeb8e8714a6e98dba5b0a5
unknown <hui.lavender@gmail.com>
2015-11-07 14:42:49 -0500

    login-0.1.0

--- dashboard/app/scripts/controllers/login.js
+++ dashboard/app/scripts/controllers/login.js
@@ -1,37 +1,40 @@
-app.controller("LoginCtrl", ['$scope', '$location', 'LoginService', 'GetUserService',
-    function($scope, $location, loginService, getUserService) {
+app.controller("LoginCtrl", ['$scope', '$location', '$http', 'AuthService', 'UserService',
+    function($scope, $location, $http, authService, userService) {
         $scope.visible = false;
         $scope.errorMsg = '';
         $scope.userName = "darth";
         $scope.password = "jedi";
+        $('body').addClass('login-body');
 
         $scope.login = function() {
-            var request = {
+            var user = {
                 userName: $scope.userName,
                 password: $scope.password
             };
-            loginService.login($scope, request);
-            $scope.$on('login', function(event, userName) {
-                getUserService.getUser($scope, userName);
-            });
-            $scope.$on("getUser", function(event, user) {
-                setUser($scope, user);
-                $location.path("/root/work");
-                $('body').removeClass('login-body');
+
+            userService.login(user).then(function(success) {
+                $scope.errorMsg = "";
+                $scope.visible = false;
+                $scope.getUser();
+            }, function(error) {
+                $scope.errorMsg = "incorrect information, please try again";
+                $scope.visible = true;
             });
         }
+
+        $scope.getUser = function() {
+            var user = {
+                "query": {
+                    "user": $scope.userName
+                }
+            };
+            userService.getUser(user).then(function(result) {
+                authService.WriteCookie(result.data.name);
+                $('body').removeClass('login-body');
+                $location.path("/root/work");
+            });
+
+        }
+
     }
 ]);
-
-function setUser(scope, user) {
-    scope.WriteCookie(user);
-    scope.user=user;
-}
-
-function getUser(scope, user) {
-    return scope.ReadCookie(user);
-}
-
-function removeUser(scope) {
-    scope.RemoveCookie(scope.user);
-}
--- dashboard/app/scripts/controllers/work.js
+++ dashboard/app/scripts/controllers/work.js
@@ -1,44 +1,9 @@
-// app.controller("ImageController",  ['$scope', 'GetImageService', 
-//  function($scope, getImageService){
-// 	$scope.loadPage = function() {
-// 	getImageService.getImages();
-// 	$scope.$on("getImage", function(event, image) {
-// 		console.log(image);
-//             });
-// }
-
-// 	// $scope.images = []
-
-// 	// $scope.errors = function(){
-// 	// 	return GetImageService.showErrors();
-// 	// };
-
-// 	// $scope.addImage = function(){
-// 	// 	GetImageService.addNewImage($scope.new_image);
-// 	// 	$scope.new_image = '';		
-// 	// }
-
-// 	// $scope.removeImage = function($index){
-// 	// 	GetImageService.removeImage($index);
-// 	// }
-// }
-// ]);
-
 app.controller("ImageController", ['$scope', 'GetImageService', 
     function($scope, getImageService) {
-
-       
-		console.log("page");
 		getImageService.getImages($scope);
 		$scope.$on("getImage", function(event, images) {
-			console.log(images);
 			$scope.display_images = images.image;
             });
       
     }
 ]);
-
-$(document).ready(function () {
-	console.log("loadpage");
-});
-
