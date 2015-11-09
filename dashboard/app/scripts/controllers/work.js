31d7379938e1dbcf43a46c926f48e30e510de80c
kun zhang <zhangkun2008204185@gmail.com>
2015-11-09 15:40:11 -0500

    update

--- dashboard/app/scripts/controllers/work.js
+++ dashboard/app/scripts/controllers/work.js
@@ -23,22 +23,49 @@
 // 	// }
 // }
 // ]);
+    // addNewImage : function(info){
+    //     var inArray = false;
+    //     for (var x in images){
+    //         if (angular.lowercase(images[x].name) == angular.lowercase(info.name)){
+    //             inArray = true;
+    //             errors = true;
+    //         }
+    //     }
+    //     if (inArray === false && info.name !== ''){
+    //         console.log(info);
+    //         images.push({
+    //             id: info.id,
+    //             title: info.title,
+    //             author: info.author,
+    //             like: info.like,
+    //             comment: info.comment,
+    //             image: info.image
+    //         });
+    //         errors = false;
+    //     }
+    // }
 
-app.controller("ImageController", ['$scope', 'GetImageService', 
-    function($scope, getImageService) {
+    // showErrors : function(){
+    //     return errors;
+    // }
 
-       
-		console.log("page");
-		getImageService.getImages($scope);
-		$scope.$on("getImage", function(event, images) {
-			console.log(images);
-			$scope.display_images = images.image;
+    // removeImage : function($index){
+    //     images.splice($index, 1);
+    // }
+    //         return "getImage";
+    //     }
+    // }
+app.controller("ImageController", ['$scope','$http',
+    function($scope, $http) {
+		 $http({
+                method: 'GET',
+                url: 'http://localhost:3000/api/work',
+            }).then(function successCallback(success) {
+                
+			$scope.display_images = success.data.works;
+            }, function errorCallback(error) {
             });
       
     }
 ]);
-
-$(document).ready(function () {
-	console.log("loadpage");
-});
 
