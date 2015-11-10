c332dab555b391648dcc568313313e66ce7e0ef9
kun zhang <zhangkun2008204185@gmail.com>
2015-11-10 17:15:39 -0500

    update

--- dashboard/app/scripts/controllers/work.js
+++ dashboard/app/scripts/controllers/work.js
@@ -1,61 +1,3 @@
-
-// app.controller("ImageController",  ['$scope', 'GetImageService', 
-//  function($scope, getImageService){
-// 	$scope.loadPage = function() {
-// 	getImageService.getImages();
-// 	$scope.$on("getImage", function(event, image) {
-// 		console.log(image);
-//             });
-// }
-
-// 	// $scope.images = []
-
-// 	// $scope.errors = function(){
-// 	// 	return GetImageService.showErrors();
-// 	// };
-
-// 	// $scope.addImage = function(){
-// 	// 	GetImageService.addNewImage($scope.new_image);
-// 	// 	$scope.new_image = '';		
-// 	// }
-
-// 	// $scope.removeImage = function($index){
-// 	// 	GetImageService.removeImage($index);
-// 	// }
-// }
-// ]);
-    // addNewImage : function(info){
-    //     var inArray = false;
-    //     for (var x in images){
-    //         if (angular.lowercase(images[x].name) == angular.lowercase(info.name)){
-    //             inArray = true;
-    //             errors = true;
-    //         }
-    //     }
-    //     if (inArray === false && info.name !== ''){
-    //         console.log(info);
-    //         images.push({
-    //             id: info.id,
-    //             title: info.title,
-    //             author: info.author,
-    //             like: info.like,
-    //             comment: info.comment,
-    //             image: info.image
-    //         });
-    //         errors = false;
-    //     }
-    // }
-
-    // showErrors : function(){
-    //     return errors;
-    // }
-
-    // removeImage : function($index){
-    //     images.splice($index, 1);
-    // }
-    //         return "getImage";
-    //     }
-    // }
 app.controller("ImageController", ['$scope','$http',
     function($scope, $http) {
 		 $http({
@@ -79,10 +21,7 @@ app.controller("ImageController", ['$scope','$http',
 
 			var add_item = {};
 
-
-
 			$scope.display_images.push($scope.add_item);
-
 
 			$scope.add_item = '';  
 			$scope.isVisible = false;
@@ -108,11 +47,6 @@ app.controller("ImageController", ['$scope','$http',
 			  	$scope.display_images.splice(index, 1);
 			  	$scope.isdeleteVisible = !$scope.isdeleteVisible;
  	  		};
-
- 	 	 // $scope.Submit_delete = function($index){
-		  	//  $scope.display_images.splice($index, 1);
-		  	//  $scope.isdeleteVisible = !$scope.isdeleteVisible;
- 	  	// };
 
 		$scope.Cancel_delete = function () {
 
