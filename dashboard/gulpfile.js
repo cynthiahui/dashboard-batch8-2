fd93281c0e0342f96e3d31055983e9152e1d1182
kun zhang <zhangkun2008204185@gmail.com>
2015-11-12 10:35:34 -0500

    update

--- dashboard/gulpfile.js
+++ dashboard/gulpfile.js
@@ -6,7 +6,7 @@ var jshint = require('gulp-jshint');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
-//sass to css with auto prefixer
+//1)sass to css with auto prefixer
 gulp.task('sass', function() {
     return gulp.src('./app/assets/sass/*.scss')
         .pipe(sass())
@@ -17,15 +17,7 @@ gulp.task('sass', function() {
 });
 
 
-
-// styles task
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/sass/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./app/assets/styless/'));
-});
-
-
+//2)minify images in src and save it build folder
 gulp.task('imagemin', () => {
     return gulp.src('./app/assets/images/src/**/*')
         .pipe(imagemin({
@@ -34,10 +26,13 @@ gulp.task('imagemin', () => {
         .pipe(gulp.dest('./app/assets/images/build'));
 });
 
+//3)clean all the minify images in build
+gulp.task('clean', function() {
+    return del(['./app/assets/images/bulid']);
+});
 
-
-//lint javascript code
-gulp.task('lint', function() {
+//4)lint error and warning in javascript code
+gulp.task('jshint', function() {
   return gulp.src('./app/scripts/*/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
@@ -45,7 +40,7 @@ gulp.task('lint', function() {
 
 
 
-// watch task
+//5) watch and monitor image and sass files
 gulp.task('watch', function() {
   gulp.watch('./app/assets/images/src/**/*', ['imagemin']);
   gulp.watch('./app/assets/sass/*.scss', ['sass']);
