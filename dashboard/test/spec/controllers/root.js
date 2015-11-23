4fa374d3e7af06a3754d48777ef2db03eab0e2ca
Chi Jin <jinchi2013us@gmail.com>
2015-11-22 23:16:01 -0500

    test added

---
+++ dashboard/test/spec/controllers/root.js
@@ -0,0 +1,47 @@
+describe('Root Controller:', function(){
+	beforeEach(module('myApp'));
+	describe('logoutCtrl', function () {
+		var logoutCtrl, scope, location, AuthService;
+		beforeEach(inject(
+			function($controller, $rootScope, $location, _AuthService_){
+
+				location = $location;
+
+				AuthService = _AuthService_ ;
+
+				scope = $rootScope.$new();
+
+				logoutCtrl = $controller('logoutCtrl', {$scope: scope});
+
+			}));
+
+		it('Should be the date is defined', function(){
+			expect(scope.date).toBeDefined();
+		});
+
+		describe('when click logout btn', function(){
+
+			describe('first the remove the cookie', function(){
+				
+				it('the RemoveCookie function should be called', function (){
+					spyOn(AuthService, 'RemoveCookie');
+					scope.logout();
+					expect(AuthService.RemoveCookie).toHaveBeenCalled();
+				});
+			});
+ 
+			
+
+			describe('and then redirect to the login page', function(){
+
+				it('the location path should be called', function(){
+					spyOn(location, 'path');
+					scope.logout();
+					expect(location.path).toHaveBeenCalledWith('/');
+				});
+			});
+
+		});
+	
+	});
+});
---
+++ dashboard/test/spec/services/authenticate.js
@@ -0,0 +1,40 @@
+describe('AuthService', function(){
+	beforeEach(module('myApp'));
+	var AuthService;
+	var rootScope;
+	var cookieStore;
+	beforeEach(inject(function($rootScope, $cookieStore, _AuthService_){
+		AuthService = _AuthService_;
+		rootScope = $rootScope;
+		cookieStore = $cookieStore;
+	}));
+
+	describe('check process of AuthService', function(){
+
+		var fake_user = 'cj';
+
+		beforeEach(function(){
+			AuthService.WriteCookie(fake_user);
+		});
+
+
+		it('the user should be save in the cookie and return true in CheckAuth', function(){
+
+			expect(AuthService.ReadCookie()).toEqual(fake_user);
+
+			expect(AuthService.CheckAuth()).toBe(true);
+
+		});
+
+		it('the user should be removed and return false in the CheckAuth', function(){
+
+			AuthService.RemoveCookie();
+			
+			expect(rootScope.user).not.toBeDefined();
+
+			expect(AuthService.CheckAuth()).toBe(false);
+		});
+
+	});
+
+});
